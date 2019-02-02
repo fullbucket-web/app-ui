2223c0c18348076105c38b82550c8f5946f4a336
Thomas Judd-Cooper <thomasjc247@gmail.com>
2019-02-02 20:36:42 +0000

    Heroku Setup

---
+++ build/precache-manifest.dd90d793baf21e747e887ae460f04ccf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f00f6c2a1f384ea7c8eb",
+    "url": "/static/js/main.f00f6c2a.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "f00f6c2a1f384ea7c8eb",
+    "url": "/static/css/main.2d1028bc.chunk.css"
+  },
+  {
+    "revision": "50007e590c1065cc074a8533691e05b9",
+    "url": "/index.html"
+  }
+];
